934f88cbdb94a729e8b7391a9f03e4eee211eff8
jannatun-nur <jannatunur03@gmail.com>
2024-01-12 09:43:21 +0600

    navbaar 30%

--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -1,26 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-[#AD6A34] text-white">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
         <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
+          <ul className="flex gap-4">
+            <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/">Home</Link></li>
+            <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/about">About Us</Link></li>
+            <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/signin">Sign In</Link></li>
+          </ul>
         </div>
       </div>
     </div>
