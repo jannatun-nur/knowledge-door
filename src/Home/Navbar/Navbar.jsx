86103db47587916266a1b13ac6dc7746762f9bec
jannatun-nur <jannatunur03@gmail.com>
2024-01-12 09:51:15 +0600

    navbaar 30%

--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-[#AD6A34] text-white">
+      <div className="navbar bg-[#AD6A34] text-white gap-4 lg:gap-0">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className=" text-white bg-[#AD6A34]">Knowledge <span className="text-[#AD6A34] bg-white px-1 lg:px-2 rounded-tr-box">
+            Door</span></a>
         </div>
         <div className="flex-none">
-          <ul className="flex gap-4">
+          <ul className="flex gap-1 lg:gap-4">
             <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/">Home</Link></li>
             <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/about">About Us</Link></li>
             <li className="hover:text-[#AD6A34] hover:bg-white px-2"><Link to="/signin">Sign In</Link></li>
