b19ec6ffa027f456ecfe87be0d51879afb8bbf3a
jannatun-nur <jannatunur03@gmail.com>
2024-01-14 22:53:23 +0600

    items 100%

--- src/Home/Writer/Writer.jsx
+++ src/Home/Writer/Writer.jsx
@@ -6,30 +6,30 @@ const Writer = () => {
               <p className=' w-32 lg:w-2/12 mx-auto my-4 l lg:my-6 text-sm lg:text-2xl bg-[#AD6A34] text-white text-center rounded-tr-xl shadow-lg'> Masterpices Writers</p>
             <div className="grid grid-cols-3 lg:grid-cols-6 gap-2">
                 <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/g4GjrQ5/shawk.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Shakespeare</p>
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/g4GjrQ5/shawk.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">Shakespeare</p>
                 </section>
-                <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/7gMc8NS/conan.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Canon</p>
+                <section className=" rounded-lg flex  bg-amber-600 py-1 gap-3">
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/7gMc8NS/conan.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">arthur canon doyle</p>
                 </section>
-                <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/bHfKk3J/michel.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Michel Madhusudhan Datt</p>
+                <section className=" rounded-lg flex  bg-amber-600 py-1 gap-3">
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/bHfKk3J/michel.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">Michel Madhusudhan Datt</p>
                 </section>
-                <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/v3Sm9sX/kazi.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Kazi Nazrul Islam</p>
+                <section className=" rounded-lg flex  bg-amber-600 py-1 gap-3">
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/v3Sm9sX/kazi.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">Kazi Nazrul Islam</p>
                 </section>
-                <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/DRqSKM1/tagore.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Rabidranath Tagore</p>
+                <section className=" rounded-lg flex  bg-amber-600 py-1 gap-3">
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/DRqSKM1/tagore.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">Rabidranath Tagore</p>
                 </section>
-                <section className="rounded-lg flex bg-amber-600 py-1 gap-3">
-                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/Vmq7GDF/jibon.jpg" alt="image" />
-                    <p className="text-gray-800 mt-5">Jibonanodo Das</p>
+                <section className=" rounded-lg flex  bg-amber-600 py-1 gap-3">
+                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/Vmq7GDF/jibon.jpg" alt="image" />
+                    <p className="text-gray-800 lg:mt-5 text-xs lg:text-xl mt-4 ">Jibonanodo Das</p>
                 </section>
-               <p className="text-2xl text-gray-800 text-center"> Coming soon..........</p>
+               <p className="text-xs lg:text-2xl text-gray-800 text-center"> Coming soon..........</p>
             </div>
         
             
