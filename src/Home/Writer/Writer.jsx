a7b1fc77ed5a6ffe48cedae6aed679f47053bfe0
jannatun-nur <jannatunur03@gmail.com>
2024-01-14 22:41:56 +0600

    items 50%

--- src/Home/Writer/Writer.jsx
+++ src/Home/Writer/Writer.jsx
@@ -3,30 +3,30 @@
 const Writer = () => {
     return (
         <div>
-            <div className="grid grid-cols-3 lg:grid-cols-6">
-                <section>
-                    <img className="w-10 h-10 lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/g4GjrQ5/shawk.jpg" alt="image" />
-                    <p className="">Shakespeare</p>
+            <div className="grid grid-cols-3 lg:grid-cols-6 gap-2">
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/g4GjrQ5/shawk.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Shakespeare</p>
                 </section>
-                <section>
-                    <img src="https://i.ibb.co/7gMc8NS/conan.jpg" alt="image" />
-                    <p>Canon</p>
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/7gMc8NS/conan.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Canon</p>
                 </section>
-                <section>
-                    <img src="https://i.ibb.co/bHfKk3J/michel.jpg" alt="image" />
-                    <p>Michel Madhusudhan Datt</p>
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/bHfKk3J/michel.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Michel Madhusudhan Datt</p>
                 </section>
-                <section>
-                    <img src="https://i.ibb.co/v3Sm9sX/kazi.jpg" alt="image" />
-                    <p>Kazi Nazrul Islam</p>
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/v3Sm9sX/kazi.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Kazi Nazrul Islam</p>
                 </section>
-                <section>
-                    <img src="https://i.ibb.co/DRqSKM1/tagore.jpg" alt="image" />
-                    <p>Rabidranath Tagore</p>
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/DRqSKM1/tagore.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Rabidranath Tagore</p>
                 </section>
-                <section>
-                    <img src="https://i.ibb.co/Vmq7GDF/jibon.jpg" alt="image" />
-                    <p>Jibonanodo Das</p>
+                <section className="flex bg-blue-300 py-1 gap-3">
+                    <img className="lg:w-16 lg:h-16 rounded-full" src="https://i.ibb.co/Vmq7GDF/jibon.jpg" alt="image" />
+                    <p className="text-gray-800 mt-5">Jibonanodo Das</p>
                 </section>
                
             </div>
