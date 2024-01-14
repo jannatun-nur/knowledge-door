33d48ef66e987effb0a220be6d197d9c5c2c7587
jannatun-nur <jannatunur03@gmail.com>
2024-01-14 18:39:27 +0600

    service 40%

--- src/Home/Service/Service.jsx
+++ src/Home/Service/Service.jsx
@@ -1,9 +1,18 @@
 
-
+import { MdGroups2 } from "react-icons/md";
 const Service = () => {
     return (
         <div>
+            Our services
+           <div>
+            <section className="bg-gray-800 w-4/12">
+            <p className="text-6xl lg:ml-48"><MdGroups2 /></p> 
+                <p className="text-center">Find Partner</p>
             
+            </section>
+
+            
+            </div> 
         </div>
     );
 };
