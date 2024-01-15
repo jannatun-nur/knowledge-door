d272841de2f95501690ce9bdb49f0189f1d7ec56
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 11:25:51 +0600

    faq 60%

--- src/Home/Faq/Faq.jsx
+++ src/Home/Faq/Faq.jsx
@@ -3,8 +3,8 @@
 const Faq = () => {
     return (
         <div>
-            <p>People's most searched question</p>
-            <div className="collapse collapse-plus bg-base-200">
+            <p className=' w-32 lg:w-2/12 mx-auto my-4 l lg:my-6 text-sm lg:text-2xl bg-[#AD6A34] text-white text-center rounded-tr-xl shadow-lg'>People's most searched question</p>
+            <div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34]">
   <input type="radio" name="my-accordion-3" checked="checked" /> 
   <div className="collapse-title text-xl font-medium">
   How do I buy a book?
@@ -13,7 +13,7 @@ const Faq = () => {
     <p>To buy a book users often need to create an account, then have to sign in .</p>
   </div>
 </div>
-<div className="collapse collapse-plus bg-base-200">
+<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2">
   <input type="radio" name="my-accordion-3" /> 
   <div className="collapse-title text-xl font-medium">
   What kind of books can I find?
@@ -22,7 +22,7 @@ const Faq = () => {
     <p>We are trying update to get all kinds of books. </p>
   </div>
 </div>
-<div className="collapse collapse-plus bg-base-200">
+<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2">
   <input type="radio" name="my-accordion-3" /> 
   <div className="collapse-title text-xl font-medium">
   Can I lve chat with reading partner?
