22b0f7d356f4bb116093d54f245526d6b0bf4e50
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 18:43:28 +0600

    contact 100%

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -9,18 +9,18 @@ const Contact = () => {
             <p className=" w-32 lg:w-2/12 mx-auto my-4 l lg:my-6 text-sm lg:text-2xl bg-[#AD6A34] text-white text-center rounded-tr-xl shadow-lg">Catch Us</p>
             <div className="text-[#AD6A34] border border-[#AD6A34] rounded-lg lg:w-8/12 mx-auto my-2">
                 <p className="flex gap-2 justify-center">
-                <p className="text-2xl mt-1"><FaPhone /></p>
-                <p  className="text-2xl">+6965260</p>
+                <p className="text- xl lg:text-2xl mt-1"><FaPhone /></p>
+                <p  className="text- xl lg:text-2xl">+6965260</p>
                 </p>
                 {/*  */}
                 <p className="flex gap-2 justify-center">
-                <p className="text-2xl mt-1"><MdEmail /></p>
-                <p  className="text-2xl">knowledgedoor@gmail.com</p>
+                <p className="text- xl lg:text-2xl mt-1"><MdEmail /></p>
+                <p  className="text- xl lg:text-2xl">knowledgedoor@gmail.com</p>
                 </p>
                 {/*  */}
                 <p className="flex gap-2 justify-center">
-                <p className="text-2xl mt-1"><AiOutlineGlobal/></p>
-                <p  className="text-2xl">www.knowledgedoor.com</p>
+                <p className="text- xl lg:text-2xl mt-1"><AiOutlineGlobal/></p>
+                <p  className="text- xl lg:text-2xl">www.knowledgedoor.com</p>
                 </p>
 
             </div>
