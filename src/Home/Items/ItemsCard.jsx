a9bfa8187aa1cd04c3ffb9009013d5c7480cf361
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 20:02:56 +0600

    items 60%

--- src/Home/Items/ItemsCard.jsx
+++ src/Home/Items/ItemsCard.jsx
@@ -4,14 +4,14 @@ const ItemsCard = ( {book}) => {
     const {name ,image,title ,price } = book;
 
     return (
-        <div>
+        <div className=" w-7/12  border border-[#AD6A34] py-4 rounded-lg shadow-lg">
           <div className="flex justify-center gap-8">
-            <section>
+            <section className=""> 
             <img className="lg:h-28" src={image} alt="" />
             </section>
-            <section>
-                <p>{name}</p>
-                <p>{title} </p>
+            <section className="text-gray-800">
+                <p className="text-sm lg:text-xl">{name}</p>
+                <p className="font-semibold">{title} </p>
                 <p>{price} </p>
                 <button>hi</button><button>hi</button>
             </section>
