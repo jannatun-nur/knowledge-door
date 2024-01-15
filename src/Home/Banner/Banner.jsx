23e16c05970e038037a3cfa2167c9dd856cd22ab
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 15:10:38 +0600

    contact 20%

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -38,24 +38,24 @@ const Banner = () => {
     // </Carousel>
     <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src={slider1} className="w-full h-[400px]" />
+    <img src={slider1} className="w-full h-[200px] lg:h-[400px]" />
     <div className="  absolute flex justify-between transform -translate-y-1/2 bottom-5 left-5 right-5 ">
-      <a href="#slide4" className=" btn btn-circle">❮</a> 
-      <a href="#slide2" className=" btn btn-circle">❯</a>
+      <a href="#slide4" className="">❮</a> 
+      <a href="#slide2" className="">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src={slider2} className="w-full  h-[400px]" />
+    <img src={slider2} className="w-full  h-[200px] lg:h-[400px]" />
     <div className="absolute flex justify-between transform -translate-y-1/2 bottom-5 left-5 right-5">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
+      <a href="#slide1" className="">❮</a> 
+      <a href="#slide3" className="">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src={slider3} className="w-full  h-[400px]" />
+    <img src={slider3} className="w-full  h-[200px] lg:h-[400px]" />
     <div className="absolute flex justify-between transform -translate-y-1/2 bottom-5 left-5 right-5">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+      <a href="#slide2" className="">❮</a> 
+      <a href="#slide1" className="">❯</a>
     </div>
   </div> 
  
--- src/Home/Faq/Faq.jsx
+++ src/Home/Faq/Faq.jsx
@@ -4,30 +4,30 @@ const Faq = () => {
     return (
         <div>
             <p className=' w-32 lg:w-2/12 mx-auto my-4 l lg:my-6 text-sm lg:text-2xl bg-[#AD6A34] text-white text-center rounded-tr-xl shadow-lg'>People's most searched question</p>
-            <div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34]">
+            <div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] w-9/12 mx-auto">
   <input type="radio" name="my-accordion-3" checked="checked" /> 
-  <div className="collapse-title text-xl font-medium">
+  <div className="collapse-title text-xs lg:text-xl font-medium">
   How do I buy a book?
   </div>
-  <div className="collapse-content"> 
+  <div className="collapse-content text-xs lg:text-xl"> 
     <p>To buy a book users often need to create an account, then have to sign in .</p>
   </div>
 </div>
-<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2">
+<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2  w-9/12 mx-auto">
   <input type="radio" name="my-accordion-3" /> 
-  <div className="collapse-title text-xl font-medium">
+  <div className="collapse-title text-xs lg:text-xl font-medium">
   What kind of books can I find?
   </div>
-  <div className="collapse-content"> 
+  <div className="collapse-content text-xs lg:text-xl"> 
     <p>We are trying update to get all kinds of books. </p>
   </div>
 </div>
-<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2">
+<div className="collapse collapse-plus bg-white text-[#AD6A34] border border-[#AD6A34] mt-2  w-9/12 mx-auto">
   <input type="radio" name="my-accordion-3" /> 
-  <div className="collapse-title text-xl font-medium">
+  <div className="collapse-title text-xs lg:text-xl font-medium">
   Can I lve chat with reading partner?
   </div>
-  <div className="collapse-content"> 
+  <div className="collapse-content text-xs lg:text-xl"> 
     <p>Sure, you can exchange your thought and opinions</p>
   </div>
 </div>
