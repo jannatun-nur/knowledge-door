ddb98572cce5ccebf6cb1456a9e7888e7d1d56d1
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 11:21:28 +0600

    faq 40%

--- src/Home/Faq/Faq.jsx
+++ src/Home/Faq/Faq.jsx
@@ -3,31 +3,32 @@
 const Faq = () => {
     return (
         <div>
+            <p>People's most searched question</p>
             <div className="collapse collapse-plus bg-base-200">
   <input type="radio" name="my-accordion-3" checked="checked" /> 
   <div className="collapse-title text-xl font-medium">
-    Click to open this one and close others
+  How do I buy a book?
   </div>
   <div className="collapse-content"> 
-    <p>hello</p>
+    <p>To buy a book users often need to create an account, then have to sign in .</p>
   </div>
 </div>
 <div className="collapse collapse-plus bg-base-200">
   <input type="radio" name="my-accordion-3" /> 
   <div className="collapse-title text-xl font-medium">
-    Click to open this one and close others
+  What kind of books can I find?
   </div>
   <div className="collapse-content"> 
-    <p>hello</p>
+    <p>We are trying update to get all kinds of books. </p>
   </div>
 </div>
 <div className="collapse collapse-plus bg-base-200">
   <input type="radio" name="my-accordion-3" /> 
   <div className="collapse-title text-xl font-medium">
-    Click to open this one and close others
+  Can I lve chat with reading partner?
   </div>
   <div className="collapse-content"> 
-    <p>hello</p>
+    <p>Sure, you can exchange your thought and opinions</p>
   </div>
 </div>
         </div>
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Banner/Banner";
 
 import Books from "./Books/Books"
+import Faq from "./Faq/Faq";
 import ItemsCard from "./Items/ItemsCard";
 import Service from "./Service/Service";
 import Writer from "./Writer/Writer";
@@ -13,7 +14,9 @@ const Home = () => {
            <Books/>
            <Writer/>
            <ItemsCard/>
+         
            <Service/> 
+           <Faq/>
         </div>
     );
 };
