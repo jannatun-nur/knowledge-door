907eef704b18984f407326f3e136006905956ad3
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 10:56:06 +0600

    about 60%

--- src/Home/Banner/About/About.jsx
+++ src/Home/Banner/About/About.jsx
@@ -4,17 +4,17 @@ const About = () => {
   return (
     <div>
       <div className="flex justify-between text-gray-800">
-        <section>
-          <p className="">knowledge Door</p>
+        <section className="text-center">
+          <p className="text-[#AD6A34] lg:text-2xl ">knowledge Door</p>
           <p>About Us</p>
-          <p>
+          <p className="lg:w-5/12 lg:ml-72">
             Online libraries have become integral in the digital age,
             revolutionizing the way people access and interact with information.
             They offer a wealth of resources, including e-books, academic
             papers, journals, and multimedia content, all accessible from the
             comfort of one's home. Knoeledge Door update this facility for bookworms.    
           </p>
-          <button className="px-3 bg-[#AD6A34] text-white">Learn More</button>
+          <button className="px-3 bg-[#AD6A34] text-white my-2">Learn More</button>
         </section>
         <section className="w-1/2">
           <img src={gif} alt="" />
