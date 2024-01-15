7060a03883fcb89d8ba327b7b2d4fef6ced2d61f
jannatun-nur <jannatunur03@gmail.com>
2024-01-15 20:07:10 +0600

    items 70%

--- src/Home/Items/ItemsCard.jsx
+++ src/Home/Items/ItemsCard.jsx
@@ -1,19 +1,20 @@
 
 
 const ItemsCard = ( {book}) => {
-    const {name ,image,title ,price } = book;
+    const {name ,image,title ,price,button ,button2} = book;
 
     return (
         <div className=" w-7/12  border border-[#AD6A34] py-4 rounded-lg shadow-lg">
           <div className="flex justify-center gap-8">
             <section className=""> 
-            <img className="lg:h-28" src={image} alt="" />
+            <img className="lg:h-32" src={image} alt="" />
             </section>
             <section className="text-gray-800">
                 <p className="text-sm lg:text-xl">{name}</p>
                 <p className="font-semibold">{title} </p>
                 <p>{price} </p>
-                <button>hi</button><button>hi</button>
+                <button className="text-white bg-[#AD6A34] rounded-lg px-4">{button}</button>
+                <button  className="text-[#AD6A34] rounded-lg px-4">{button2}</button>
             </section>
           </div>
         </div>
